be1d55ba31bd74444daac9c737d016f9ee8f1722
Yunus <yunus.rectangle951@silomails.com>
2025-02-10 00:56:46 +0700

    Update TUnit.Assertions

--- src/KuliJob.Tests/GlobalUsings.cs
+++ src/KuliJob.Tests/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.DependencyInjection;
 global using Assert = TUnit.Assertions.Assert;
+global using TUnit.Assertions.AssertConditions.Throws;
 
 [assembly: Parallelizable(ParallelScope.All)]
