f4f44d84407339fab50cf74ec52419a6e58dc0b7
Yunus <yunus.rectangle951@silomails.com>
2025-02-15 21:51:06 +0700

    Try fix test

--- src/KuliJob.Tests/ParallelJobsTests.cs
+++ src/KuliJob.Tests/ParallelJobsTests.cs
@@ -13,21 +13,24 @@ public class ParallelJobsTests : BaseTest
         });
         var jobStorage = ss.Services.GetRequiredService<IJobStorage>();
         var now = DateTimeOffset.UtcNow.AddMilliseconds(1);
-        var jobId1 = await ss.QueueJob.Enqueue("handler_job", now);
-        var jobId2 = await ss.QueueJob.Enqueue("delay_handler_job", now, new JobDataMap()
-        {
-            { "delay", 1000 },
-        });
-        var jobId3 = await ss.QueueJob.Enqueue("handler_job", now);
-        var jobId4 = await ss.QueueJob.Enqueue("delay_handler_job", now, new JobDataMap()
-        {
-            { "delay", 1000 },
-        });
-        await WaitJobTicks();
-        var job1 = await jobStorage.GetJobById(jobId1);
-        var job2 = await jobStorage.GetJobById(jobId2);
-        var job3 = await jobStorage.GetJobById(jobId3);
-        var job4 = await jobStorage.GetJobById(jobId4);
+        var jobIds = await Task.WhenAll([
+            ss.QueueJob.Enqueue("handler_job", now),
+            ss.QueueJob.Enqueue("delay_handler_job", now, new JobDataMap()
+            {
+                { "delay", 1500 },
+            }),
+            ss.QueueJob.Enqueue("handler_job", now),
+            ss.QueueJob.Enqueue("delay_handler_job", now, new JobDataMap()
+            {
+                { "delay", 1500 },
+            }),
+        ]);
+        await WaitJobTicks(2);
+        var jobs = await Task.WhenAll(jobIds.Select(v => jobStorage.GetJobById(v)));
+        var job1 = jobs[0];
+        var job2 = jobs[1];
+        var job3 = jobs[2];
+        var job4 = jobs[3];
         await Assert.That(job1!.JobState).IsEqualTo(JobState.Completed);
         await Assert.That(job2!.JobState).IsEqualTo(JobState.Active);
         await Assert.That(job3!.JobState).IsEqualTo(JobState.Completed);
@@ -36,9 +39,9 @@ public class ParallelJobsTests : BaseTest
         await Assert.That(job1And3Delta).IsBetween(TimeSpan.Zero, TimeSpan.FromMilliseconds(30))
             .Or
             .IsBetween(TimeSpan.Zero, TimeSpan.FromMilliseconds(175)).Because("After EF Core changes");
-        await Task.Delay(1100);
-        job2 = await jobStorage.GetJobById(jobId2);
-        job4 = await jobStorage.GetJobById(jobId4);
+        await Task.Delay(1600);
+        job2 = await jobStorage.GetJobById(job2.Id);
+        job4 = await jobStorage.GetJobById(job4.Id);
         await Assert.That(job2!.JobState).IsEqualTo(JobState.Completed);
         await Assert.That(job4!.JobState).IsEqualTo(JobState.Completed);
     }
