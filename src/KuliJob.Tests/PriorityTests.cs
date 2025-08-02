40304d9b169853d74b47ec58229ab22c14ab2482
Yunus <19399214+yunusefendi52@users.noreply.github.com>
2025-08-02 23:53:15 +0700

    asdf

--- src/KuliJob.Tests/PriorityTests.cs
+++ src/KuliJob.Tests/PriorityTests.cs
@@ -37,7 +37,7 @@ public class PriorityTests : BaseTest
             var job1 = priorityJobs.First()!.CompletedOn!.Value.ToUnixTimeMilliseconds();
             var job2 = priorityJobs.Last()!.CompletedOn!.Value.ToUnixTimeMilliseconds();
             var deltaPriority = job2 - job1;
-            await Assert.That(deltaPriority).IsLessThan(50);
+            await Assert.That(deltaPriority).IsLessThan(250);
         }
         // Priority 1
         {
@@ -90,7 +90,7 @@ public class PriorityTests : BaseTest
             var job1 = priorityJobs.First(v => v!.Priority == 0)!.CompletedOn!.Value.ToUnixTimeMilliseconds();
             var job2 = priorityJobs.First(v => v!.Priority == 1)!.CompletedOn!.Value.ToUnixTimeMilliseconds();
             var deltaPriority = job2 - job1;
-            await Assert.That(deltaPriority).IsLessThan(50);
+            await Assert.That(deltaPriority).IsLessThan(250);
         }
     }
 }
